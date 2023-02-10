469250d2d4394b39de108b9f3cb631d70da4daa2
lilianaba <52921112+lilianaba@users.noreply.github.com>
2023-02-10 20:51:06 +1030

    some more styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,123 +1,3 @@
-// import Form from 'react-bootstrap/Form';
-// import Button from 'react-bootstrap/Button';
-// import Col from 'react-bootstrap/Col';
-// import InputGroup from 'react-bootstrap/InputGroup';
-// import Row from 'react-bootstrap/Row'
-// import { Formik } from 'formik';
-
-// // const { Formik } =formik;
-
-// const schema = yup.object().shape({
-//     name: yup.string().required(),
-//     email: yup.string().required(),
-//     message: yup.string().required(),
-//   });
-
-// const Contact =() => {
-//     return (
-//     //     <div className='form-container'>
-//     //     <Form>
-//     //     <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-//     //       <Form.Label>Name</Form.Label>
-//     //       <Form.Control type="email" placeholder="John Smith" />
-//     //     </Form.Group>
-//     //     <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
-//     //       <Form.Label>Email address</Form.Label>
-//     //       <Form.Control type="email" placeholder="name@example.com" />
-//     //     </Form.Group>
-//     //     <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-//     //       <Form.Label>Example textarea</Form.Label>
-//     //       <Form.Control as="textarea" rows={3} />
-//     //     </Form.Group>
-//     //   </Form>
-
-//     //   </div>
-
-//     <div>
-//         <Formik
-//       validationSchema={schema}
-//       onSubmit={console.log}
-//     //   initialValues={{
-//     //     name: 'Mark',
-//     //     lastName: 'Otto',
-//     //     username: '',
-
-//     //   }}
-//     >
-//       {({
-//         handleSubmit,
-//         handleChange,
-//         handleBlur,
-//         values,
-//         touched,
-//         isValid,
-//         errors,
-//       }) => (
-//         <Form noValidate onSubmit={handleSubmit}>
-//           <Row className="mb-3">
-//             <Form.Group
-//               as={Col}
-//               md="4"
-//               controlId="validationFormik101"
-//               className="position-relative"
-//             >
-//               <Form.Label>Name</Form.Label>
-//               <Form.Control
-//                 type="text"
-//                 name="name"
-//                 value={values.name}
-//                 onChange={handleChange}
-//                 // isValid={touched.name && !errors.firstName}
-//               />
-//               <Form.Control.Feedback tooltip>Looks good!</Form.Control.Feedback>
-//             </Form.Group>
-//             <Form.Group
-//               as={Col}
-//               md="4"
-//               controlId="validationFormik102"
-//               className="position-relative"
-//             >
-//               <Form.Label>Email</Form.Label>
-//               <Form.Control
-//                 type="text"
-//                 name="email"
-//                 value={values.email}
-//                 onChange={handleChange}
-//                 // isValid={touched.lastName && !errors.lastName}
-//               />
-
-//               <Form.Control.Feedback tooltip>Looks good!</Form.Control.Feedback>
-//             </Form.Group>
-//             <Form.Group as={Col} md="4" controlId="validationFormikUsername2">
-//               <Form.Label>Message</Form.Label>
-//               <InputGroup hasValidation>
-//                 <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-//                 <Form.Control
-//                   type="text"
-//                   placeholder="message"
-//                   aria-describedby="inputGroupPrepend"
-//                   name="username"
-//                   value={values.username}
-//                   onChange={handleChange}
-//                 //   isInvalid={!!errors.username}
-//                 />
-//                 <Form.Control.Feedback type="invalid" tooltip>
-//                   {errors.message}
-//                 </Form.Control.Feedback>
-//               </InputGroup>
-//             </Form.Group>
-//           </Row>
-
-//           <Button type="submit">Submit form</Button>
-//         </Form>
-//       )}
-//     </Formik>
-//     </div>
-//         )
-// }
-
-// export default Contact;
-
 import React from "react";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import { useState } from "react";
@@ -126,54 +6,73 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { validateEmail } from "../utils/helper";
 
-// const styles = {
-//   container: {
-//     marginTop: "65px",
-//     display: "flex",
-//   },
-//   left: {
-//     border: "1px solid black",
-//     overflow: "hidden",
-//     textAlign: "center",
-//     backgroundColor: "rgb(244, 242, 242)",
-//   },
-//   right: {
-//     minWidth: "75%",
-//     marginTop: "60px",
-//     paddingRight: "50px",
-//   },
-// };
 const Contact = () => {
   const [response, setResponse] = useState(false);
-  const [validated, setValidated] = useState(false);
+
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const { name, email, message } = formState;
+
   const handleSubmit = (e) => {
-    const form = e.currentTarget;
-    if (form.checkValidity() === false) {
-      e.preventDefault();
-      e.stopPropagation();
+    e.preventDefault();
+    if (!errorMessage) {
+      console.log("Submit Form", formState);
     }
-    setValidated(true);
-    setResponse(true);
   };
+
+  const [errorMessage, setErrorMessage] = useState("");
+  const handleChange = (e) => {
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage("Your email is invalid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+      console.log("Handle Form", formState);
+    }
+  };
+
   return (
     <div>
-      <Container fluid>
+      <Container>
         <Row className="pad3">
-          <Col >
+          <Col>
             <div className="pinky">
               <h1>Contact Me</h1>
             </div>
           </Col>
           <Col xs={9}>
-            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form noValidate onSubmit={handleSubmit}>
               <Row className="sm-12">
                 <FloatingLabel
                   controlId="floatingInput"
                   label="Your Name"
                   className="mb-3"
                 >
-                  <Form.Control required type="text" placeholder="Your name" />
+                  <Form.Control
+                    name="name"
+                    onBlur={handleChange}
+                    defaultValue={name}
+                    required
+                    type="text"
+                    placeholder="Your name"
+                  />
                   <Form.Control.Feedback type="invalid">
                     Please provide a valid name.
                   </Form.Control.Feedback>
@@ -186,6 +85,9 @@ const Contact = () => {
                   className="mb-3"
                 >
                   <Form.Control
+                    name="email"
+                    onBlur={handleChange}
+                    defaultValue={email}
                     type="email"
                     placeholder="Your e-mail"
                     required
@@ -196,8 +98,14 @@ const Contact = () => {
                 </FloatingLabel>
               </Row>
               <Row className="sm-12">
-                <FloatingLabel controlId="floatingTextarea2" label="Please write your message here">
+                <FloatingLabel
+                  controlId="floatingTextarea2"
+                  label="Please write your message here"
+                >
                   <Form.Control
+                    name="message"
+                    onBlur={handleChange}
+                    defaultValue={message}
                     as="textarea"
                     placeholder="Please write your message here"
                     required
@@ -208,21 +116,21 @@ const Contact = () => {
                   </Form.Control.Feedback>
                 </FloatingLabel>
               </Row>
-              <Row className="pad3"> 
-              <Button
-                className="d-grid gap-2 btn-cus"
-                type="submit"
-                 size="lg"
-              >
-                Send me your message
-              </Button>
+              <Row className="pad3">
+                <Button
+                  className="d-grid gap-2 btn-cus"
+                  type="submit"
+                  size="lg"
+                >
+                  Send me your message
+                </Button>
               </Row>
             </Form>
-            <div>
-              {response && (
-                <span>
-                  Please note, this has not been connected to a backend. :)
-                </span>
+            <div className="pin">
+              {errorMessage && (
+                <div>
+                  <p className="pinky">{errorMessage}</p>
+                </div>
               )}
             </div>
           </Col>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,5 +1,111 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import importPDF from '../Assets/pdf/Resume.pdf'
+import { useState } from 'react';
+
 const Resume = () => {
-    return (<div> "this is the Resume"</div>)
+ const [showPDF, setShowPDF ] = useState (true);
+ const handleClick = ()=>{
+    setShowPDF(!showPDF);
+ }
+ const skills = [
+    {
+      id: "1",
+      name: "HTML",
+      icon: "fa fa-angle-double-right",
+    },
+     {
+      id: "2",
+      name: "CSS",
+      icon: " fa fa-angle-double-right",
+    },
+    {
+      id: "3",
+      name: "JavaScript",
+      icon: "fa fa-angle-double-right",
+    },
+    {
+      id: "4",
+      name: "API",
+      icon: "fa fa-angle-double-right",
+    },
+    {
+      id: "5",
+      name: "3rd Party API",
+      icon: " fa fa-angle-double-right",
+    },
+    {
+      id: "6",
+      name: "Mysql Database",
+      icon: "fa fa-angle-double-right",
+    },
+    {
+      id: "7",
+      name: "Node.js",
+      icon: "fa fa-angle-double-right",
+    },
+     {
+      id: "8",
+      name: "NoSQL Database",
+      icon: " fa fa-angle-double-right",
+    },
+    {
+      id: "9",
+      name: "Progressive",
+      icon: "fa fa-angle-double-right",
+    },
+    {
+      id: "10",
+      name: "React",
+      icon: "fa fa-angle-double-right",
+    },
+    {
+      id: "11",
+      name: "MERN",
+      icon: " fa fa-angle-double-right",
+    },
+    {
+      id: "12",
+      name:"GitHub",
+      icon:"fa-brands fa-github",
+    },
+    {
+      id:"13",
+      name:"Branches",
+      icon:"fa-sharp fa-solid fa-merge",
+
+    }
+    
+ ]
+    
+
+    return (
+    // <div>
+    <Container fluid>  
+        <h3 className='pinky pad3'>Resume</h3>
+        <h6 className='pinky'>
+          Print or download my{' '}
+          <a  className ="pinky" href="#resumePDF" onClick={handleClick}>
+            resume
+          </a>
+        </h6>
+  
+        {showPDF ? (
+        <Row className='skillList' md={3} sm={2} lg={5}>
+            {skills.map((skill) => (
+        <Col>
+          <div className='d-flex  pinky'> <i className= {`${skill.icon} px-2`}  ></i> <p> {skill.name}</p></div>
+        </Col>))}
+      </Row>):(
+        <div className='w-100'> 
+        <object width="100%" height="1000" data= {importPDF} type="application/pdf"/>
+        </div>
+      )}
+        
+    </Container> 
+    // </div>
+    );
 }
 
 export default Resume;
